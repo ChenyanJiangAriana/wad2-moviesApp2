94852ea5391eac0ae4c7cc24debb2f9eb5b9ced5
Chenyan Jiang <624071310@qq.com>
2020-12-15 15:08:49 +0000

    finish Route-based code splitting

--- src/login/Hero.js
+++ src/login/Hero.js
@@ -1,31 +1,22 @@
 import React,{lazy,Suspense} from "react";
-//import "src/node_modules/bootstrap/dist/css/bootstrap.css";
-import HomePage from "../pages/homePage";
-import MoviePage from '../pages/movieDetailsPage';
 import {BrowserRouter, Route, Redirect, Switch} from "react-router-dom"    // CHANGED
-import FavoriteMoviesPage from '../pages/favoritesMoviesPage';       // NEW
-import MovieReviewPage from "../pages/movieReviewPage";
-import SiteHeader from '../components/siteHeader';
-import UpcomingMoviePage from '../pages/UpcomingMoviePage';
 import MoviesContextProvider from "../contexts/moviesContext";
 import GenresContextProvider from "../contexts/genresContext";
-import AddMovieReviewPage from '../pages/addMovieReviewPage';
-import WatchListMoviesPage from '../pages/watchListMoviesPage';
-
-import NowPlayingMoviesPage from '../pages/nowPlayingMoviesPage';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import RecommendationsPage from  '../pages/recommendationsPage';
-import PersonPage from '../pages/personDetailsPage';
-//import PopularPeoplePage from '../pages/popularPeoplePage';
 import PeopleContextProvider from "../contexts/peopleContext";
 const PopularPeoplePage= lazy(() => import('../pages/popularPeoplePage'));
 const SlideNowPlaying= lazy(() => import("../components/slideNowPlaying"));
 const TopRatedMoviesPage=lazy(() =>import('../pages/topRatedMoviesPage'));
-
-//import ReactStars from "react-rating-stars-component";
-//import withRouter from 'react-router-dom';
-//import ReactDOM from "react-dom";
-//import PrivateRouter from "../pages/PrivateRouter";
+const NowPlayingMoviesPage=lazy(() =>import('../pages/nowPlayingMoviesPage'));
+const WatchListMoviesPage=lazy(() =>import("../pages/watchListMoviesPage"));
+const AddMovieReviewPage =lazy(() =>import("../pages/addMovieReviewPage"));
+const PersonPage= lazy(() => import("../pages/personDetailsPage"));
+const UpcomingMoviePage= lazy(() => import( '../pages/UpcomingMoviePage'));
+const SiteHeader= lazy(() => import('../components/siteHeader'));
+const MovieReviewPage= lazy(() => import("../pages/movieReviewPage"));
+const FavoriteMoviesPage= lazy(() => import("../pages/favoritesMoviesPage"));  
+const HomePage= lazy(() => import("../pages/homePage"));
+const MoviePage= lazy(() => import('../pages/movieDetailsPage')); 
 export const Hero =({handleLogout})=>{
     return(
 <div>
@@ -60,7 +51,6 @@ export const Hero =({handleLogout})=>{
                         <Route path="/movies/Upcoming" component={UpcomingMoviePage} />
                         <Route path="/person/:id" component={PersonPage} />
                         <Route path="/movies/:id" component={MoviePage} />
-                        <Route path="/movies/recommendations/:id" component={RecommendationsPage} />
                         <Route path="/" component={HomePage} />
                         <Redirect from="*" to="/" />
                     </Switch>
